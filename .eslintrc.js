c9b69fa5a7c39d213ada62a8b6b3b9d681786a4d
insanetim <47120724+insanetim@users.noreply.github.com>
2023-10-02 11:44:06 +0300

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         html: {
           component: 'always',
-          normal: 'always',
+          normal: 'never',
           void: 'always'
         },
         svg: 'always'
